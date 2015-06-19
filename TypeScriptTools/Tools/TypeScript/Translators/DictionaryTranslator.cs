9d569910767ab40ba5a389b5fbcaebd1c8bfdc7e
Everton Araujo <everton.basso@gmail.com>
2015-06-19 15:56:58 -0300

    updated version for publishing

---
+++ TypeScriptTools/Tools/TypeScript/Translators/DictionaryTranslator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Timple.Tools.TypeScript.Translators
+{
+  public class DictionaryTranslator : ITypeScriptPipelineTranslator
+  {
+    public void Prepare(TypeScriptPipelineTranslator translator) {
+      translator.AddTailTranslator(this);
+    }
+
+    public bool Accepts(Type tp) {
+      return tp.IsGenericType && tp.GetGenericArguments().Length == 2
+        && (typeof(IDictionary<,>).MakeGenericType(tp.GetGenericArguments()).IsAssignableFrom(tp));
+    }
+
+    public string Translate(Type tp, TypeScriptPipelineTranslator translator) {
+      return "any";
+    }
+  }
+}
